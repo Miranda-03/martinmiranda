cbc133f814acae40e87376c29c59ee1f12c3de4e
alumno <alumno@ipm-13.lan>
2021-09-06 17:02:10 -0300

    ejercicio para la practicar no terminado

---
+++ laboratorio/java/src/com/company/EjercicioPractica/Main.java
@@ -0,0 +1,13 @@
+package com.company.EjercicioPractica;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static HashMap<String, int> meses;
+
+    public static void main(String[] args) {
+
+
+    }
+}
