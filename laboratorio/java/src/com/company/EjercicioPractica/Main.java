08993a19a4cf3d07a10ebd60b14107f9150fcc67
Miranda-03 <martin.miranda.2004@gmail.com>
2021-09-20 01:26:57 -0300

    ejercicio del 23 de agosto

--- laboratorio/java/src/com/company/EjercicioPractica/Main.java
+++ laboratorio/java/src/com/company/EjercicioPractica/Main.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Main {
 
-    public static HashMap<String, int> meses;
+    public static HashMap<String, Integer> meses;
 
     public static void main(String[] args) {
 
