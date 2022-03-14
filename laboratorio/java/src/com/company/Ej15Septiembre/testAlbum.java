be184677b40850f64c5f790e3d67230620d6ee60
Miranda-03 <martin.miranda.2004@gmail.com>
2022-03-14 02:35:38 -0300

    ordene el repositorio

--- laboratorio/java/src/com/company/Ej15Septiembre/testAlbum.java
+++
@@ -1,5 +0,0 @@
-package com.company.Ej15Septiembre;
-
-public class testAlbum {
-
-}
--- laboratorio/java/src/com/company/Practica10Noviembre/Main.java
+++
@@ -1,7 +0,0 @@
-package com.company.Practica10Noviembre;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
